d69d8e9d50dd196acd977fe12d83656758879829
DominikWolos <dominik.wolos@bitbag.pl>
2021-12-16 12:41:21 +0100

    Fixed SecurityBundle ver and removed comments

--- src/DependencyInjection/Compiler/ExtendedTypesPass.php
+++ src/DependencyInjection/Compiler/ExtendedTypesPass.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the `liip/LiipImagineBundle` project.
- *
- * (c) https://github.com/liip/LiipImagineBundle/graphs/contributors
- *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
- */
-
 namespace BitBag\SyliusAgreementPlugin\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -21,9 +12,5 @@ class ExtendedTypesPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $parameter = $container->getParameter('sylius_agreement_plugin.extended_form_types');
-//        dd($parameter);
-//        if (!$container->hasDefinition($liipImagineDriver)) {
-//            throw new InvalidConfigurationException(sprintf("Specified driver '%s' is not defined.", $liipImagineDriver));
-//        }
     }
 }
