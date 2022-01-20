d38739e0f573005a6dd4406f5411619ee191bc0b
DominikWolos <dominik.wolos@bitbag.pl>
2022-01-20 12:41:25 +0100

    ECS fix

--- src/Form/Type/Agreement/Admin/AgreementAutocompleteChoiceType.php
+++ src/Form/Type/Agreement/Admin/AgreementAutocompleteChoiceType.php
@@ -29,8 +29,7 @@ class AgreementAutocompleteChoiceType extends AbstractType
         FormView $view,
         FormInterface $form,
         array $options
-    ): void
-    {
+    ): void {
         $view->vars['remote_criteria_type'] = 'contains';
         $view->vars['remote_criteria_name'] = 'phrase';
     }
