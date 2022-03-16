72cd1e0c8e091c6b1966a07134a5acfd7e16f9f9
liszkapawel <pawel.liszka00@gmail.com>
2022-03-16 12:33:46 +0100

    Add Form/Type/Shop phpunit tests

--- tests/Unit/Form/Type/Agreement/Shop/AgreementCollectionTypeTest.php
+++ tests/Unit/Form/Type/Agreement/Shop/AgreementCollectionTypeTest.php
@@ -1,14 +1,18 @@
 <?php
 
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
 declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusAgreementPlugin\Unit\Form\Type\Agreement\Shop;
 
-use BitBag\SyliusAgreementPlugin\Entity\Agreement\AgreementTranslationInterface;
-use BitBag\SyliusAgreementPlugin\Form\Type\Agreement\Shop\AgreementType;
 use BitBag\SyliusAgreementPlugin\Form\Type\Agreement\Shop\AgreementCollectionType;
 use BitBag\SyliusAgreementPlugin\Repository\AgreementRepositoryInterface;
-use Closure;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Sylius\Bundle\ResourceBundle\Form\Type\FixedCollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,41 +21,80 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 final class AgreementCollectionTypeTest extends TestCase
 {
 
-    public function testConfigureOptions(): void
+    public function test_it_configures_correctly(): void
     {
-        $resolver = $this->createMock(OptionsResolver::class);
-        $resolver->expects(self::exactly(3))->method('setDefault')
+        $type = new AgreementCollectionType($this->mock_agreement_repository());
+        $resolver = $this->mock_resolver();
+
+        $resolver
+            ->expects(self::exactly(3))
+            ->method('setDefault')
             ->withConsecutive(
-                ['entry_type', AgreementType::class],
-                ['entry_name', self::isInstanceOf(Closure::class)],
-                ['entry_options', self::isInstanceOf(Closure::class)]
-                )
+                [
+                    self::equalTo('entry_type'),
+                    'BitBag\SyliusAgreementPlugin\Form\Type\Agreement\Shop\AgreementType',
+                ],
+                [
+                    self::equalTo('entry_name'),
+                    self::anything(),
+                ],
+                [
+                    self::equalTo('entry_options'),
+                    self::anything(),
+                ]
+            )
             ->willReturnSelf();
 
-        $subject = new AgreementCollectionType($this->createMock(AgreementRepositoryInterface::class));
-        $subject->configureOptions($resolver);
+        $type->configureOptions($resolver);
     }
 
-    public function testBuildForm(): void
+    public function test_it_builds_correctly(): void
     {
-        $builder = $this->createMock(FormBuilderInterface::class);
-        $builder->expects(self::once())->method('addEventListener');
+        $type = new AgreementCollectionType($this->mock_agreement_repository());
+        $formBuilder = $this->mock_builder();
 
-        $subject = new AgreementCollectionType($this->createMock(AgreementRepositoryInterface::class));
-        $subject->buildForm($builder, []);
+        $formBuilder
+            ->expects(self::once())
+            ->method('addEventListener')
+            ->with(self::equalTo('form.submit'), self::anything());
+        $type->buildForm($formBuilder, []);
     }
 
-    public function testGetBlockPrefix(): void
+    public function test_it_has_correct_block_prefix(): void
     {
         $subject = new AgreementCollectionType($this->createMock(AgreementRepositoryInterface::class));
 
         self::assertEquals('bitbag_sylius_agreement_plugin_agreement_approval_collection', $subject->getBlockPrefix());
     }
 
-    public function testGetParent(): void
+    public function test_it_has_correct_parent(): void
     {
         $subject = new AgreementCollectionType($this->createMock(AgreementRepositoryInterface::class));
 
         self::assertEquals(FixedCollectionType::class, $subject->getParent());
+    }
+
+    /**
+     * @return OptionsResolver|MockObject
+     */
+    private function mock_resolver(): object
+    {
+        return $this->createMock(OptionsResolver::class);
+    }
+
+    /**
+     * @return AgreementRepositoryInterface|MockObject
+     */
+    private function mock_agreement_repository(): object
+    {
+        return $this->createMock(AgreementRepositoryInterface::class);
+    }
+
+    /**
+     * @return FormBuilderInterface|MockObject
+     */
+    private function mock_builder(): object
+    {
+        return $this->createMock(FormBuilderInterface::class);
     }
 }
--- tests/Unit/Form/Type/Agreement/Shop/AgreementHiddenTypeTest.php
+++ tests/Unit/Form/Type/Agreement/Shop/AgreementHiddenTypeTest.php
@@ -1,10 +1,17 @@
 <?php
 
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
 declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusAgreementPlugin\Unit\Form\Type\Agreement\Shop;
 
 use BitBag\SyliusAgreementPlugin\Form\Type\Agreement\Shop\AgreementHiddenType;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormInterface;
@@ -13,39 +20,58 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class AgreementHiddenTypeTest extends TestCase
 {
-    public function testGetParent(): void
+    public function test_it_has_correct_parent(): void
     {
         $subject = new AgreementHiddenType();
 
         self::assertEquals(HiddenType::class, $subject->getParent());
     }
 
-    public function testGetBlockPrefix(): void
+    public function test_it_has_correct_block_prefix(): void
     {
         $subject = new AgreementHiddenType();
 
         self::assertEquals('bitbag_sylius_agreement_plugin_agreement_approval_hidden', $subject->getBlockPrefix());
     }
 
-    public function testBuildView(): void
+    public function test_it_builds_view_correctly(): void
     {
         $subject = new AgreementHiddenType();
-
         $view = new FormView();
 
-        $subject->buildView($view, $this->createMock(FormInterface::class), ['extended_label' => 'test1']);
+        $subject->buildView($view, $this->mock_form(), [
+            'extended_label' => 'test1'
+        ]);
+
         self::assertEquals('test1', $view->vars['extended_label'],);
     }
 
-    public function testConfigureOptions(): void
+    public function test_it_configures_correctly(): void
     {
-        $resolver = $this->createMock(OptionsResolver::class);
+        $subject = new AgreementHiddenType();
+        $resolver = $this->mock_resolver();
+
         $resolver
             ->expects(self::once())
             ->method('setRequired')
             ->with('extended_label');
 
-        $subject = new AgreementHiddenType();
         $subject->configureOptions($resolver);
+    }
+
+    /**
+     * @return FormInterface|MockObject
+     */
+    private function mock_form(): object
+    {
+        return $this->createMock(FormInterface::class);
+    }
+
+    /**
+     * @return OptionsResolver|MockObject
+     */
+    private function mock_resolver(): object
+    {
+        return $this->createMock(OptionsResolver::class);
     }
 }
