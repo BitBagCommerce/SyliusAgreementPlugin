4a4e9c7625a82814ea1a9d698b6ea59cf7273729
liszkapawel <pawel.liszka00@gmail.com>
2022-05-05 15:44:43 +0200

    Move determineState function to new class and spec it

---
+++ src/Resolver/AgreementStateResolverInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusAgreementPlugin\Resolver;
+
+use BitBag\SyliusAgreementPlugin\Entity\Agreement\AgreementHistoryInterface;
+use BitBag\SyliusAgreementPlugin\Entity\Agreement\AgreementInterface;
+
+interface AgreementStateResolverInterface
+{
+    public function resolve(
+        AgreementHistoryInterface $agreementHistory,
+        AgreementInterface $submittedAgreement,
+        string $resolvedAgreementHistoryState
+    ): string;
+}
